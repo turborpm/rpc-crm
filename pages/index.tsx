2cc40d500e5bbc97f09315752d91ffccce3d9a4e
galortega <galfeort@espol.edu.ec>
2022-02-08 15:50:05 -0500

    Basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hola mundo</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center align-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
