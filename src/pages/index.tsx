723895000e7c83a7800f88cd8aba54c21752732a
galortega <galfeort@espol.edu.ec>
2022-02-09 16:42:31 -0500

    Make voting persist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,18 @@ const Home: NextPage = () => {
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
 
+  const voteMutation = trpc.useMutation(["cast-vote"]);
+
   if (firstPokemon.isLoading || secondPokemon.isLoading) {
     return null;
   }
 
   const voteForRoundest = (selected: number) => {
+    if (selected === first)
+      voteMutation.mutate({ votedFor: first, votedAgainst: second });
+    else if (selected === second)
+      voteMutation.mutate({ votedFor: second, votedAgainst: first });
+      
     updateIds(getOptionsForVote());
   };
 
