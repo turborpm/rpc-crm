39ccc0dbc6f1e85d33ab0093d8c897e4dcc0b459
galortega <galfeort@espol.edu.ec>
2022-02-10 10:15:34 -0500

    Add percent round count to results page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -24,18 +24,22 @@ const getPokemonInOrder = async () =>
 
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
 
-const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number] }> = (
-  props
-) => {
+const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) return 0;
+  return ((VoteFor / (VoteFor + VoteAgainst)) * 100).toFixed(2);
+};
+
+const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number] }> = ({
+  pokemon,
+}) => {
   return (
-    <div className="flex border-b p-2 items-center">
-      <Image
-        src={props.pokemon.spriteUrl}
-        layout="fixed"
-        width={64}
-        height={64}
-      />
-      <div className="capitalze">{props.pokemon.name}</div>
+    <div className="flex border-b p-2 items-center justify-between">
+      <div className="flex items-center">
+        <Image src={pokemon.spriteUrl} layout="fixed" width={64} height={64} />
+        <div className="capitalze">{pokemon.name}</div>
+      </div>
+      <div className="pr-1">{generateCountPercent(pokemon)}%</div>
     </div>
   );
 };
@@ -46,7 +50,7 @@ const ResultsPage: React.FC<{
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl">Results</h2>
-      <div className="p-2"/>
+      <div className="p-2" />
       <div className="flex flex-col w-full max-w-2xl border">
         {props.pokemon.map((currentPokemon, index) => (
           <PokemonListing pokemon={currentPokemon} key={index} />
