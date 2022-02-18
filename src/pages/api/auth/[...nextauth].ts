7a3781686af58331681fa213c1339d4d998216ff
galortega <galfeort@espol.edu.ec>
2022-02-17 23:05:16 -0500

    Securing a preview deployment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,36 +7,41 @@ const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 
 export default NextAuth({
   // Configure one or more authentication providers
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "jsmith",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize() {
-        return {
-          id: 1,
-          name: "J Smith",
-          email: "jsmith@example.com",
-          image:  "https://lh3.googleusercontent.com/a/AATXAJwCDUo1jIT5HDujh9XXrPfV-1Vnj64VvCtSu7MQUrE=s96-c",
-        };
-      },
-    }),
-    GoogleProvider({
-      clientId: `${GOOGLE_CLIENT_ID}`,
-      clientSecret: `${GOOGLE_CLIENT_SECRET}`,
-    }),
-    FacebookProvider({
-      clientId: `${process.env.FACEBOOK_ID}`,
-      clientSecret: `${process.env.FACEBOOK_SECRET}`,
-    }),
-    // ...add more providers here
-  ],
+  providers:
+    process.env.VERCEL_ENV === "preview"
+      ? [
+          CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              username: {
+                label: "Username",
+                type: "text",
+                placeholder: "jsmith",
+              },
+              password: { label: "Password", type: "password" },
+            },
+            async authorize() {
+              return {
+                id: 1,
+                name: "J Smith",
+                email: "jsmith@example.com",
+                image:
+                  "https://lh3.googleusercontent.com/a/AATXAJwCDUo1jIT5HDujh9XXrPfV-1Vnj64VvCtSu7MQUrE=s96-c",
+              };
+            },
+          }),
+        ]
+      : [
+          GoogleProvider({
+            clientId: `${GOOGLE_CLIENT_ID}`,
+            clientSecret: `${GOOGLE_CLIENT_SECRET}`,
+          }),
+          FacebookProvider({
+            clientId: `${process.env.FACEBOOK_ID}`,
+            clientSecret: `${process.env.FACEBOOK_SECRET}`,
+          }),
+          // ...add more providers here
+        ],
   pages: {
     signIn: "/auth/signin",
   },
